f0e7b3e526ab6b6a3f9b879e42d202e4525b0da7
parketh <yeungparkhay@gmail.com>
2024-10-03 18:35:11 +0100

    fix configs and deps

--- app/config.ts
+++ app/config.ts
@@ -2,24 +2,28 @@ import { http, createConfig } from "wagmi";
 import { sepolia, optimismSepolia } from "wagmi/chains";
 import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
 import { defineChain } from "viem";
-import optimismSDK from "@eth-optimism/sdk";
 
-export const {
-  NEXT_PUBLIC_L2_RPC_URL,
-  NEXT_PUBLIC_L2_WS_URL,
-  NEXT_PUBLIC_L1_CHAIN_ID,
-  NEXT_PUBLIC_L2_CHAIN_ID,
-  NEXT_PUBLIC_L2_EXPLORER_URL,
-  NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
-  NEXT_PUBLIC_ADDRESS_MANAGER,
-  NEXT_PUBLIC_L1_CROSS_DOMAIN_MESSANGER_PROXY,
-  NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY,
-  NEXT_PUBLIC_OPTIMISM_PORTAL_PROXY,
-  NEXT_PUBLIC_L2_OUTPUT_ORACLE_PROXY,
-  NEXT_PUBLIC_L2_STANDARD_BRIDGE,
-} = process.env;
+const NEXT_PUBLIC_L2_RPC_URL = process.env.NEXT_PUBLIC_L2_RPC_URL as string;
+const NEXT_PUBLIC_L2_WS_URL = process.env.NEXT_PUBLIC_L2_WS_URL as string;
+const NEXT_PUBLIC_L1_CHAIN_ID = process.env.NEXT_PUBLIC_L1_CHAIN_ID as string;
+const NEXT_PUBLIC_L2_CHAIN_ID = process.env.NEXT_PUBLIC_L2_CHAIN_ID as string;
+const NEXT_PUBLIC_L2_EXPLORER_URL = process.env
+  .NEXT_PUBLIC_L2_EXPLORER_URL as string;
+const NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID = process.env
+  .NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string;
+const NEXT_PUBLIC_L1_CROSS_DOMAIN_MESSANGER_PROXY = process.env
+  .NEXT_PUBLIC_L1_CROSS_DOMAIN_MESSANGER_PROXY as string;
+const NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY = process.env
+  .NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY as string;
+const NEXT_PUBLIC_L1_OPTIMISM_PORTAL_PROXY = process.env
+  .NEXT_PUBLIC_L1_OPTIMISM_PORTAL_PROXY as string;
+const NEXT_PUBLIC_DISPUTE_GAME_FACTORY = process.env
+  .NEXT_PUBLIC_DISPUTE_GAME_FACTORY as string;
+const NEXT_PUBLIC_L2_STANDARD_BRIDGE_PROXY = process.env
+  .NEXT_PUBLIC_L2_STANDARD_BRIDGE_PROXY as string;
 
 if (!NEXT_PUBLIC_L2_RPC_URL) {
+  console.log({ NEXT_PUBLIC_L2_RPC_URL });
   throw new Error("NEXT_PUBLIC_L2_RPC_URL is not set");
 }
 if (!NEXT_PUBLIC_L2_WS_URL) {
@@ -37,23 +41,20 @@ if (!NEXT_PUBLIC_L2_EXPLORER_URL) {
 if (!NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID) {
   throw new Error("NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID is not set");
 }
-if (!NEXT_PUBLIC_ADDRESS_MANAGER) {
-  throw new Error("NEXT_PUBLIC_ADDRESS_MANAGER is not set");
-}
 if (!NEXT_PUBLIC_L1_CROSS_DOMAIN_MESSANGER_PROXY) {
   throw new Error("NEXT_PUBLIC_L1_CROSS_DOMAIN_MESSANGER_PROXY is not set");
 }
 if (!NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY) {
   throw new Error("NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY is not set");
 }
-if (!NEXT_PUBLIC_OPTIMISM_PORTAL_PROXY) {
-  throw new Error("NEXT_PUBLIC_OPTIMISM_PORTAL_PROXY is not set");
+if (!NEXT_PUBLIC_L1_OPTIMISM_PORTAL_PROXY) {
+  throw new Error("NEXT_PUBLIC_L1_OPTIMISM_PORTAL_PROXY is not set");
 }
-if (!NEXT_PUBLIC_L2_OUTPUT_ORACLE_PROXY) {
-  throw new Error("NEXT_PUBLIC_L2_OUTPUT_ORACLE_PROXY is not set");
+if (!NEXT_PUBLIC_DISPUTE_GAME_FACTORY) {
+  throw new Error("NEXT_PUBLIC_DISPUTE_GAME_FACTORY is not set");
 }
-if (!NEXT_PUBLIC_L2_STANDARD_BRIDGE) {
-  throw new Error("NEXT_PUBLIC_L2_STANDARD_BRIDGE is not set");
+if (!NEXT_PUBLIC_L2_STANDARD_BRIDGE_PROXY) {
+  throw new Error("NEXT_PUBLIC_L2_STANDARD_BRIDGE_PROXY is not set");
 }
 
 export const tohma = defineChain({
@@ -79,12 +80,7 @@ export const chains = [sepolia, tohma, optimismSepolia] as const;
 
 export const config = createConfig({
   chains: chains,
-  connectors: [
-    injected(),
-    walletConnect({ projectId: NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID }),
-    metaMask(),
-    safe(),
-  ],
+  connectors: [],
   transports: {
     [sepolia.id]: http(),
     [tohma.id]: http(),
@@ -92,28 +88,23 @@ export const config = createConfig({
   },
 });
 
-const zeroAddr = "0x".padEnd(42, "0");
-
 export const opStackL1Contracts = {
-  StateCommitmentChain: zeroAddr,
-  CanonicalTransactionChain: zeroAddr,
-  BondManager: zeroAddr,
-  AddressManager: NEXT_PUBLIC_ADDRESS_MANAGER,
   L1CrossDomainMessenger: NEXT_PUBLIC_L1_CROSS_DOMAIN_MESSANGER_PROXY,
   L1StandardBridge: NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY,
-  OptimismPortal: NEXT_PUBLIC_OPTIMISM_PORTAL_PROXY,
-  L2OutputOracle: NEXT_PUBLIC_L2_OUTPUT_ORACLE_PROXY,
+  OptimismPortal: NEXT_PUBLIC_L1_OPTIMISM_PORTAL_PROXY,
+  DisputeGameFactory: NEXT_PUBLIC_DISPUTE_GAME_FACTORY,
 };
 
-export const bridges = {
-  Standard: {
-    l1Bridge: NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY,
-    l2Bridge: NEXT_PUBLIC_L2_STANDARD_BRIDGE,
-    Adapter: optimismSDK.StandardBridgeAdapter,
-  },
-  ETH: {
-    l1Bridge: NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY,
-    l2Bridge: NEXT_PUBLIC_L2_STANDARD_BRIDGE,
-    Adapter: optimismSDK.ETHBridgeAdapter,
-  },
+export {
+  NEXT_PUBLIC_L2_RPC_URL,
+  NEXT_PUBLIC_L2_WS_URL,
+  NEXT_PUBLIC_L1_CHAIN_ID,
+  NEXT_PUBLIC_L2_CHAIN_ID,
+  NEXT_PUBLIC_L2_EXPLORER_URL,
+  NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
+  NEXT_PUBLIC_L1_CROSS_DOMAIN_MESSANGER_PROXY,
+  NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY,
+  NEXT_PUBLIC_L1_OPTIMISM_PORTAL_PROXY,
+  NEXT_PUBLIC_DISPUTE_GAME_FACTORY,
+  NEXT_PUBLIC_L2_STANDARD_BRIDGE_PROXY,
 };
